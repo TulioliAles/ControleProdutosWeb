21ec7822ed9b60217b85ef4439f4f8c41fa0e9f5
alessandro <desmecad@hotmail.com>
2020-03-21 17:11:10 -0300

    Commit 1 - criado component Navbar e view Home. Iniciada a view Cadastro.

---
+++ src/views/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div class="jumbotron">
+            <h1 class="display-3">Bem vindo!</h1>
+            <p class="lead">Este é seu sistema, utilize a barra de navegação para acessar as páginas</p>
+            <hr class="my-4"/>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Cadastrar</a>
+            </p>
+        </div>
+    )
+}
+
+export default Home
