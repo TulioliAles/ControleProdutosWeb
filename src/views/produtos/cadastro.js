2ec5f254c1a399c58de3bb8926da1a7894722fbe
alessandro <desmecad@hotmail.com>
2020-03-23 20:24:29 -0300

    Commit 5 - Implementadas regras para salvar dados inseridos nos campos - Salvando no Local Storage do browser temporariamente

--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import ProdutoService from '../../app/produtoService'
+
 const estadoInicial = {
         nome: '',
         sku: '',
@@ -11,6 +13,11 @@ const estadoInicial = {
 export default class CadastroProduto extends React.Component {
     
     state = estadoInicial
+
+    constructor(){
+        super()
+        this.service = new ProdutoService()
+    }
     
     onChange = (event) => {
         const valor = event.target.value
@@ -19,7 +26,16 @@ export default class CadastroProduto extends React.Component {
     }
     
     onSubmit = (event) => {
-        console.log(this.state)
+        const produto = {
+            nome: this.state.nome,
+            sku: this.state.sku,
+            descricao: this.state.descricao,
+            preco: this.state.preco,
+            fornecedor: this.state.fornecedor
+        }
+        this.service.salvar(produto)
+        this.limpaCampos()
+        console.log('Salvo com sucesso')
     }
 
     limpaCampos = () => {
