4fa2109d27832c698175a6068c0ea1f231e20283
alessandro <desmecad@hotmail.com>
2020-03-24 22:22:45 -0300

    Commit 7 - Atribuidos links no menu de navegação das páginas

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React from 'react'
 import Navbar from './components/navbar'
 import Rotas from './rotas'
 
+import { HashRouter } from 'react-router-dom'
+
 function App() {
   return (
-    <>
+    <HashRouter>
       <div className="container">
           <Navbar />
           <Rotas />
       </div>
-    </>
+    </HashRouter>
   );
 }
 
-export default App;
+export default App
